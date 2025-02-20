5c0c7bc4c65090a42a04ebf5764ca94301e8fce7
Alex Garbarino <alexiothebrave@gmail.com>
2025-02-19 19:54:15 -0600

    all passing

---
+++ aa17-strings/problems/02-multiline-strings.py
@@ -0,0 +1,21 @@
+# It's time to write your first multiline of code in Python. Use the `print` to
+# send a multiline message with a quote in it to the terminal.
+
+# Here is an example of using print to display message to the user:
+
+print('''In this code include "quotes"
+of any kind because they won't get confused with
+the end of the string!''')
+
+# Display your own message using print below:
+
+print('''"Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not.
+      It’s not a story the Jedi would tell you.
+      It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful
+      and so wise he could use the Force to influence the midichlorians to
+      create life… He had such a knowledge of the dark side that he could even
+      keep the ones he cared about from dying. The dark side of the Force is a pathway
+      to many abilities some consider to be unnatural. He became so powerful…
+      the only thing he was afraid of was losing his power, which eventually, of course, he did.
+      Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him
+      in his sleep. Ironic. He could save others from death, but not himself."''')
