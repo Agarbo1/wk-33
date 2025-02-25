26701252b2e9094924fe785e870a9c4f2fb8a75e
Alex Garbarino <alexiothebrave@gmail.com>
2025-02-24 21:43:15 -0600

    commit to add all files

---
+++ aa17-statements/problems/01-try-except.py
@@ -0,0 +1,21 @@
+# In Python, error handling can be done using a `try`/`except` block. Implement
+# `except` blocks to handle the exceptions that will be raised for the following
+# `try` blocks:
+
+# Example 1
+# try:
+#     string = 'hello'
+#     string[0] = 'm'
+#     print(str)
+# except TypeError as error:
+#     print('Strings are Immutable', error)
+# finally:
+#     print('I happen regardless of any exceptions.')
+
+# # Example 2
+# try:
+#     print(x)
+# except NameError as error:
+#     print('Variable x is not defined', error)
+# finally:
+#     print('I happen regardless of any exceptions.')
